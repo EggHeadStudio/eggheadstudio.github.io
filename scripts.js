313bae98bbdcafa149ce65eccf39c465d338423b
EggHeadStudio <74627733+EggHeadStudio@users.noreply.github.com>
2023-05-10 20:58:15 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+    $("#makeRoyaltyRecipe").click(function () {
+        const payer = $("#payer").val();
+        const amount = parseFloat($("#amount").val());
+        const vatPercentage = parseFloat($("#vat").val());
+        const perPlay = parseFloat($("#perPlay").val());
+        const recipient = $("#recipient").val();
+
+        if (!payer || !amount || !vatPercentage || !perPlay || !recipient) {
+            alert("Täytä kaikki syöttökentät.");
+            return;
+        }
+
+        const vatAmount = amount * (vatPercentage / 100);
+        const listeningTimes = amount / perPlay;
+
+        const royaltyContent = `
+        <div style="display: flex; flex-direction: column; align-items: center; width: 100%;">
+          <div style="font-size: 14px; text-align: center;">${payer}</div>
+          <div style="font-size: 14px; text-align: center;">Royalty payd: ${amount.toFixed(2)}€</div>
+          <div style="font-size: 14px; text-align: center;">VAT: ${vatAmount.toFixed(2)}€</div>
+          <div style="font-size: 14px; text-align: center;">Listening times: ${listeningTimes.toFixed(0)}</div>
+          <div style="font-size: 14px; text-align: center;">Payd to: ${recipient}</div>
+        </div>
+        `;
+
+        const royaltyElement = document.createElement('div');
+        royaltyElement.innerHTML = royaltyContent;
+
+        const opt = {
+          margin: 1,
+          filename: 'royalty-recipe.pdf',
+          image: { type: 'jpeg', quality: 0.98 },
+          html2canvas: { scale: 2 },
+          jsPDF: { unit: 'in', format: 'a4', orientation: 'portrait' },
+        };
+
+        html2pdf().set(opt).from(royaltyElement).toPdf().get('pdf').then(function (pdf) {
+          const totalPages = pdf.internal.getNumberOfPages();
+          for (let i = 1; i <= totalPages; i++) {
+            pdf.setPage(i);
+            pdf.setFontSize(12);
+            pdf.setTextColor(150);
+          }
+          window.open(pdf.output('bloburl'), '_blank');
+        });
+
+        $("#payer").val("");
+        $("#amount").val("");
+        $("#vat").val("");
+        $("#perPlay").val("0.0005");
+        $("#recipient").val("");
+    });
+});
