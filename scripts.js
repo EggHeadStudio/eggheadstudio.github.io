7187d791a9bd65d2635f6f86e5bd6e77cb542ee7
EggHeadStudio <74627733+EggHeadStudio@users.noreply.github.com>
2023-05-11 14:05:26 +0300

    Update scripts.js muokattu taas jotta toimisi mobiililla

--- scripts.js
+++ scripts.js
@@ -1,90 +1,65 @@
-function isMobileDevice() {
-  return (
-    typeof window.orientation !== "undefined" ||
-    navigator.userAgent.indexOf("IEMobile") !== -1
-  );
-}
-
 $(document).ready(function () {
-  $("#makeRoyaltyRecipe").click(function () {
-    const payer = $("#payer").val();
-    const amount = parseFloat($("#amount").val());
-    const vatPercentage = parseFloat($("#vat").val());
-    const perPlay = parseFloat($("#perPlay").val());
-    const recipient = $("#recipient").val();
-
-    if (!payer || !amount || !vatPercentage || !perPlay || !recipient) {
-      alert("Täytä kaikki syöttökentät.");
-      return;
-    }
-
-    const vatAmount = amount * (vatPercentage / 100);
-    const listeningTimes = amount / perPlay;
-
-    const royaltyContent = `
-      <div style="display: flex; flex-direction: column; align-items: center; width: 100%;">
-        <div style="font-size: 14px; text-align: center;">${payer}</div>
-        <div style="font-size: 14px; text-align: center;">Royalty payd: ${amount.toFixed(2)}€</div>
-        <div style="font-size: 14px; text-align: center;">VAT: ${vatAmount.toFixed(2)}€</div>
-        <div style="font-size: 14px; text-align: center;">Listening times: ${listeningTimes.toFixed(0)}</div>
-        <div style="font-size: 14px; text-align: center;">Payd to: ${recipient}</div>
-      </div>
-    `;
-
-    const royaltyElement = document.createElement("div");
-    royaltyElement.innerHTML = royaltyContent;
-    document.body.appendChild(royaltyElement);
-
-    const opt = {
-      margin: 1,
-      filename: "royalty-recipe.pdf",
-      image: { type: "jpeg", quality: 0.98 },
-      html2canvas: { scale: 2 },
-      jsPDF: { unit: "in", format: "a4", orientation: "portrait" },
+    function isMobileDevice() {
+        return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
     };
 
-    if (isMobileDevice()) {
-      // Mobile version
-      html2pdf()
-        .set(opt)
-        .from(royaltyElement)
-        .outputPdf()
-        .then(function (pdfBuffer) {
-          const pdfBlob = new Blob([pdfBuffer], { type: "application/pdf" });
-          saveAs(pdfBlob, "royalty-recipe.pdf");
-    
-          document.body.removeChild(royaltyElement);
-    
-          $("#payer").val("");
-          $("#amount").val("");
-          $("#vat").val("");
-          $("#perPlay").val("0.0005");
-          $("#recipient").val("");
-        });
-    } else {
-      // Desktop version
-      html2pdf()
-        .set(opt)
-        .from(royaltyElement)
-        .toPdf()
-        .get("pdf")
-        .then(function (pdf) {
-          const totalPages = pdf.internal.getNumberOfPages();
-          for (let i = 1; i <= totalPages; i++) {
-            pdf.setPage(i);
-            pdf.setFontSize(12);
-            pdf.setTextColor(150);
-          }
-          window.open(pdf.output("bloburl"), "_blank");
+    $("#makeRoyaltyRecipe").click(function () {
+        const payer = $("#payer").val();
+        const amount = parseFloat($("#amount").val());
+        const vatPercentage = parseFloat($("#vat").val());
+        const perPlay = parseFloat($("#perPlay").val());
+        const recipient = $("#recipient").val();
 
-          document.body.removeChild(royaltyElement);
+        if (!payer || !amount || !vatPercentage || !perPlay || !recipient) {
+            alert("Täytä kaikki syöttökentät.");
+            return;
+        }
 
-          $("#payer").val("");
-          $("#amount").val("");
-          $("#vat").val("");
-          $("#perPlay").val("0.0005");
-          $("#recipient").val("");
-        });
-    }
-  });
+        const vatAmount = amount * (vatPercentage / 100);
+        const listeningTimes = amount / perPlay;
+
+        const royaltyContent = `
+        <div style="display: flex; flex-direction: column; align-items: center; width: 100%;">
+          <div style="font-size: 14px; text-align: center;">${payer}</div>
+          <div style="font-size: 14px; text-align: center;">Royalty payd: ${amount.toFixed(2)}€</div>
+          <div style="font-size: 14px; text-align: center;">VAT: ${vatAmount.toFixed(2)}€</div>
+          <div style="font-size: 14px; text-align: center;">Listening times: ${listeningTimes.toFixed(0)}</div>
+          <div style="font-size: 14px; text-align: center;">Payd to: ${recipient}</div>
+        </div>
+        `;
+
+        const royaltyElement = document.createElement('div');
+        royaltyElement.innerHTML = royaltyContent;
+
+        const opt = {
+          margin: 1,
+          filename: 'royalty-recipe.pdf',
+          image: { type: 'jpeg', quality: 0.98 },
+          html2canvas: { scale: 2 },
+          jsPDF: { unit: 'in', format: 'a4', orientation: 'portrait' },
+        };
+
+        if (isMobileDevice()) {
+            html2pdf().set(opt).from(royaltyElement).toPdf().get('pdf').output('arraybuffer').then(function (pdfBuffer) {
+                const pdfBlob = new Blob([pdfBuffer], { type: 'application/pdf' });
+                saveAs(pdfBlob, 'royalty-recipe.pdf');
+            });
+        } else {
+            html2pdf().set(opt).from(royaltyElement).toPdf().get('pdf').then(function (pdf) {
+                const totalPages = pdf.internal.getNumberOfPages();
+                for (let i = 1; i <= totalPages; i++) {
+                    pdf.setPage(i);
+                    pdf.setFontSize(12);
+                    pdf.setTextColor(150);
+                }
+                window.open(pdf.output('bloburl'), '_blank');
+            });
+        }
+
+        $("#payer").val("");
+        $("#amount").val("");
+        $("#vat").val("");
+        $("#perPlay").val("0.0005");
+        $("#recipient").val("");
+    });
 });
