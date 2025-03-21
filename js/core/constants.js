ab83223d39debfe617b781af8d1a3c3c7425c627
EggHeadStudio <74627733+EggHeadStudio@users.noreply.github.com>
2025-03-21 23:42:09 +0200

    Update constants.js

--- js/core/constants.js
+++ js/core/constants.js
@@ -10,6 +10,11 @@ export const ENEMY_CHASE_SPEED = 3
 export const APPLE_THROW_SPEED = 8
 export const ROCK_SIZE = 50 // Slightly larger than bombs
 export const ROCK_COUNT = 100 // Initial number of rocks
+export const WOODEN_BOX_SIZE = 45 // Size of wooden boxes
+export const WOODEN_BOX_COUNT = 30 // Initial number of wooden boxes
+export const WOODEN_BOX_THROW_MULTIPLIER = 0.3 // Reduced from 2 to 0.5 (4x reduction)
+export const WOODEN_BOX_FLOAT_SPEED = 0.5 // How fast boxes float in water
+export const WOODEN_BOX_SNAP_DISTANCE = 60 // Distance for boxes to snap to each other
 
 export const TERRAIN_TYPES = {
   WATER: 0,
@@ -17,4 +22,3 @@ export const TERRAIN_TYPES = {
   FOREST: 2,
   DIRT: 3,
 }
-
